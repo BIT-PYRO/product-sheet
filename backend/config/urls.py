14a80809d212f7c625ce55a8e4954791d403183e
JANKI-K <apoorvadixit2204@gmail.com>
2026-03-10 14:09:51 +0530

    Add root status endpoint for deployed backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+
+def root_status(_request):
+	return JsonResponse({
+		'success': True,
+		'message': 'Product Sheet backend is running.',
+		'docs': '/api/docs/swagger/',
+	})
+
 urlpatterns = [
+	path('', root_status, name='root-status'),
 	path('admin/', admin.site.urls),
 	path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
 	path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='swagger-ui'),
